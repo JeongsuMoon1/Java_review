78091aebec3dbc18183cf302145fce7f3b26dfa5
appac <appac@125.134.157.51>
2020-06-11 22:52:21 +0900

    3. 주석 및 세미콜론 & 데이터 타입

---
+++ java_tutorials/src/org/opentutorials/javatutorials/_3_comment/_1_comment.java
@@ -0,0 +1,24 @@
+package org.opentutorials.javatutorials._3_comment;
+
+public class _1_comment {
+
+	public static void main(String[] args) {
+		
+		// 한 줄 주석 : 해당 줄의 끝까지
+		
+		// 두 개의 변수가 같은 데이터 타입일 때 아래와 같이 코드를 작성한다.
+		// String a,b;
+		
+		// <여러 줄 주석용>
+		/* 
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+		*/
+		
+		/** 자바독(javadoc) 주석 : 자바의 API문서(규약)를 만들 때 사용.
+		 * 
+		 */
+	}
+
+}
---
+++ java_tutorials/src/org/opentutorials/javatutorials/_3_comment/_2_semicollon.java
@@ -0,0 +1,27 @@
+package org.opentutorials.javatutorials._3_comment;
+
+public class _2_semicollon {
+
+	public static void main(String[] args) {
+		
+		// 세미콜론은 문장이 끝났음을 알려주는 특수한 기호
+		
+		// 문장의 예
+		// assignment statement(문장)
+//		aValue = 8933.234;
+		// increment statement(문장)
+//		aValue++;
+		// method invocation statement(문장)
+//		System.out.println("Hello World");
+		//object creation statement(문장)
+//		Bicycle myBike = new Bicycle();
+		
+		// 1. statement 끝에는 ;이 붙는다.
+		// 2. "Hello World" 같은 값은 표현식이라고 하고, 그 뒤에는 세미콜론 ;이 안붙는다.
+		// 3. {} 뒤에는 세미콜론 ; 이 안 붙는다.
+		// 4. println(); newLine(); 같은 문장에는 세미콜론 ;이 붙는다.
+		// 5. 세미콜론 ;을 사용하여 한 줄에 두 개의 문장을 사용할 수 있다.
+		// 결국 두 문장을 나누는 기준점이 바로 세미콜론이다.
+	}
+
+}
